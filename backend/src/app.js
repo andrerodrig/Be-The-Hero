627ef8493712a054fcfc8abfc1672d129f70f470
André Rodrigues <43825622+andrerodrig@users.noreply.github.com>
2020-04-03 23:00:22 -0300

    Atualizando app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,37 +10,5 @@ app.use(express.json());
 app.use(routes);
 app.use(errors());
 
-/**
- * Rota / Recurso
- */
-
-/**
- * Metodos HTTP
- * 
- * GET: Buscar uma informação do back-end
- * POST: Criar uma informação no back-end
- * PUT: Alterar uma informação no back-end
- * DELETE: Deletar uma informação no back-end
- */
-
-/**
- * Tipos de parâmetros
- * 
- * Query Params: Parâmetros noemados enviados na rota após "?"
- * Route Params: Parâmetros utilizados para identificar recursos
- * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
- */
-
-/**
- * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
- * NoSQL: MongoDB, Redis, CouchDB etc.
- */
-
-/**
- * Driver: SELECT * FROM users
- * Query Builder: table('users').select('*').where()
- */
-
-
 
 module.exports = app;
